10fbff710e70f837acc7ef27a7ccbcfd542cced9
yruano <51043000+yruano@users.noreply.github.com>
2023-07-13 21:33:44 +0900

    feat: 몬스터소환

--- Assets/Dev/Hak/Monster/Monster.cs
+++ Assets/Dev/Hak/Monster/Monster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,33 +6,46 @@ using UnityEngine;
 [System.Serializable]
 public class MonsterStat
 {
-  [SerializeField]
-  public float HP;
-  [SerializeField]
-  public float Damage;
-  [SerializeField]
-  public float Defense;
+    [SerializeField]
+    public float HP;
+    [SerializeField]
+    public float Damage;
+    [SerializeField]
+    public float Defense;
 
-  public MonsterStat()
-  {
-    HP = 10;
-    Damage = 10;
-    Defense = 5;
-  }
+    public MonsterStat()
+    {
+        HP = 10f;
+        Damage = 10f;
+        Defense = 5f;
+    }
 }
 public class Monster : MonoBehaviour
 {
-  [HideInInspector]
-  public MonsterStat Stat;
-  public MonsterSpawn Spawn;
+    private Func<IEnumerator> NextPattern;
+    private Coroutine CurrentPattern = null;
+    public MonsterStat stat;
+    private void Start()
+    {
+        NextPattern = PatternAttack;
+    }
 
-  private void Awake()
-  {
-    Spawn = new(this);
-    Spawn.AddMonsters(0);
-  }
+    private void Update()
+    {
+        if (CurrentPattern is null && NextPattern is not null && Input.GetKeyDown(KeyCode.W))
+            CurrentPattern = StartCoroutine(NextPattern());
+    }
 
-  private void Update() 
-  {  
-  }
+    private IEnumerator PatternAttack()
+    {
+        CurrentPattern = null;
+        NextPattern = PatternSkill;
+        yield return null;
+    }
+
+    private IEnumerator PatternSkill()
+    {
+        CurrentPattern = null;
+        yield return null;
+    }
 }
--- Assets/Dev/Hak/Monster/MonsterSpawn.cs
+++ Assets/Dev/Hak/Monster/MonsterSpawn.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IMonster
+public class MonsterSpawn : MonoBehaviour
 {
-  public void Attack();
-  public void Skill();
-}
+    public List<GameObject> P_Monster;
+    public Transform FirstSpawnPos;
+    public Transform SecondSpawnPos;
+    public Transform ThirdSpawnPos;
+    private List<Vector3> _pos = new();
 
-public class MonsterSpawn
-{
-  public static string[] MonsterPaths =
-  {
-    
-  };
-  public List<IMonster> Monsters = new();
-  private Monster _monster;
+    private void Awake()
+    {
+        _pos.Add(FirstSpawnPos.position);
+        _pos.Add(SecondSpawnPos.position);
+        _pos.Add(ThirdSpawnPos.position);
+    }
 
-  public MonsterSpawn(Monster monster)
-  {
-    _monster = monster;
-  }
-  public void AddMonsters(int id)
-  {
-    // Monsters.Add()
-  }
+    public void Spawn(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(P_Monster[Random.Range(0, 4)], _pos[i], Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Spawn(1);
+        }
+    }
 }
